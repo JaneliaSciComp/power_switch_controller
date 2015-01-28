0cbb59fe9ceccbc9dda71b6f8e54530cd4634b06
Peter Polidoro <peterpolidoro@gmail.com>
2015-01-28 15:57:18 -0500

    remote -> modular.

--- firmware/Callbacks.cpp
+++ firmware/Callbacks.cpp
@@ -13,7 +13,7 @@ namespace callbacks
 {
 // Callbacks must be non-blocking (avoid 'delay')
 //
-// remote_device.getParameterValue must be cast to either:
+// modular_device.getParameterValue must be cast to either:
 // char*
 // long
 // double
@@ -22,31 +22,31 @@ namespace callbacks
 //
 // For more info read about ArduinoJson v3 JsonParser JsonValues
 //
-// remote_device.getSavedVariableValue type must match the saved variable default type
-// remote_device.setSavedVariableValue type must match the saved variable default type
+// modular_device.getSavedVariableValue type must match the saved variable default type
+// modular_device.setSavedVariableValue type must match the saved variable default type
 
 
 void getLedsPoweredCallback()
 {
   boolean leds_powered = power_switch_controller.getLedsPowered();
-  remote_device.addBooleanToResponse("leds_powered",leds_powered);
+  modular_device.addBooleanToResponse("leds_powered",leds_powered);
 }
 
 void setChannelsCallback()
 {
-  long channels = remote_device.getParameterValue(constants::channels_parameter_name);
+  long channels = modular_device.getParameterValue(constants::channels_parameter_name);
   power_switch_controller.setChannels(channels);
 }
 
 void setChannelOnCallback()
 {
-  long channel = remote_device.getParameterValue(constants::channel_parameter_name);
+  long channel = modular_device.getParameterValue(constants::channel_parameter_name);
   power_switch_controller.setChannelOn(channel);
 }
 
 void setChannelOffCallback()
 {
-  long channel = remote_device.getParameterValue(constants::channel_parameter_name);
+  long channel = modular_device.getParameterValue(constants::channel_parameter_name);
   power_switch_controller.setChannelOff(channel);
 }
 
@@ -63,12 +63,12 @@ void setAllChannelsOffCallback()
 void getChannelsOnCallback()
 {
   long channels_on = power_switch_controller.getChannelsOn();
-  remote_device.addToResponse("channels_on",channels_on);
+  modular_device.addToResponse("channels_on",channels_on);
 }
 
 void getChannelCountCallback()
 {
   int channel_count = power_switch_controller.getChannelCount();
-  remote_device.addToResponse("channel_count",channel_count);
+  modular_device.addToResponse("channel_count",channel_count);
 }
 }
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,7 +4,7 @@
 #include "StandardCplusplus.h"
 #include "MemoryFree.h"
 #include "Flash.h"
-#include "RemoteDevice.h"
+#include "ModularDevice.h"
 #include "Constants.h"
 #include "Callbacks.h"
 #include "SPI.h"
@@ -13,7 +13,7 @@
 
 // See README.md for more information
 
-using namespace RemoteDevice;
+using namespace ModularDevice;
 
 void setup()
 {
@@ -21,51 +21,51 @@ void setup()
   power_switch_controller.init();
 
   // Device Info
-  remote_device.setName(constants::device_name);
-  remote_device.setModelNumber(constants::model_number);
-  remote_device.setFirmwareNumber(constants::firmware_number);
+  modular_device.setName(constants::device_name);
+  modular_device.setModelNumber(constants::model_number);
+  modular_device.setFirmwareNumber(constants::firmware_number);
 
   // Saved Variables
 
   // Parameters
-  Parameter& channels_parameter = remote_device.createParameter(constants::channels_parameter_name);
+  Parameter& channels_parameter = modular_device.createParameter(constants::channels_parameter_name);
   channels_parameter.setRange(constants::channels_min,constants::channels_max);
-  Parameter& channel_parameter = remote_device.createParameter(constants::channel_parameter_name);
+  Parameter& channel_parameter = modular_device.createParameter(constants::channel_parameter_name);
   channel_parameter.setRange(constants::channel_min,constants::channel_max);
 
   // Methods
-  Method& get_leds_powered_method = remote_device.createMethod(constants::get_leds_powered_method_name);
+  Method& get_leds_powered_method = modular_device.createMethod(constants::get_leds_powered_method_name);
   get_leds_powered_method.attachCallback(callbacks::getLedsPoweredCallback);
 
-  Method& set_channels_method = remote_device.createMethod(constants::set_channels_method_name);
+  Method& set_channels_method = modular_device.createMethod(constants::set_channels_method_name);
   set_channels_method.attachCallback(callbacks::setChannelsCallback);
   set_channels_method.addParameter(channels_parameter);
 
-  Method& set_channel_on_method = remote_device.createMethod(constants::set_channel_on_method_name);
+  Method& set_channel_on_method = modular_device.createMethod(constants::set_channel_on_method_name);
   set_channel_on_method.attachCallback(callbacks::setChannelOnCallback);
   set_channel_on_method.addParameter(channel_parameter);
 
-  Method& set_channel_off_method = remote_device.createMethod(constants::set_channel_off_method_name);
+  Method& set_channel_off_method = modular_device.createMethod(constants::set_channel_off_method_name);
   set_channel_off_method.attachCallback(callbacks::setChannelOffCallback);
   set_channel_off_method.addParameter(channel_parameter);
 
-  Method& set_all_channels_on_method = remote_device.createMethod(constants::set_all_channels_on_method_name);
+  Method& set_all_channels_on_method = modular_device.createMethod(constants::set_all_channels_on_method_name);
   set_all_channels_on_method.attachCallback(callbacks::setAllChannelsOnCallback);
 
-  Method& set_all_channels_off_method = remote_device.createMethod(constants::set_all_channels_off_method_name);
+  Method& set_all_channels_off_method = modular_device.createMethod(constants::set_all_channels_off_method_name);
   set_all_channels_off_method.attachCallback(callbacks::setAllChannelsOffCallback);
 
-  Method& get_channels_on_method = remote_device.createMethod(constants::get_channels_on_method_name);
+  Method& get_channels_on_method = modular_device.createMethod(constants::get_channels_on_method_name);
   get_channels_on_method.attachCallback(callbacks::getChannelsOnCallback);
 
-  Method& get_channel_count_method = remote_device.createMethod(constants::get_channel_count_method_name);
+  Method& get_channel_count_method = modular_device.createMethod(constants::get_channel_count_method_name);
   get_channel_count_method.attachCallback(callbacks::getChannelCountCallback);
 
   // Start Server
-  remote_device.startServer(constants::baudrate);
+  modular_device.startServer(constants::baudrate);
 }
 
 void loop()
 {
-  remote_device.handleServerRequests();
+  modular_device.handleServerRequests();
 }
