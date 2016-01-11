c5ef18da9f76440043a3a856a331ac6aa9949c29
Peter Polidoro <peterpolidoro@gmail.com>
2016-01-11 17:21:03 -0500

    Made some changes, but not working at this commit.

--- firmware/Controller.h
+++ firmware/Controller.h
@@ -20,6 +20,8 @@ public:
   Controller();
   void setup();
   void update();
+  ModularDevice::ModularServer& getModularServer();
+
   void executeStandaloneCallback();
   bool getLedsPowered();
   void saveState(int state);
@@ -47,8 +49,16 @@ public:
   uint8_t getHoldDutyIntVar();
   int getHoldDurIntVar();
 private:
-  uint32_t states_array_[constants::STATE_COUNT];
+  ModularDevice::ModularServer modular_server_;
+  SavedVariable saved_variables_[constants::SAVED_VARIABLE_COUNT_MAX];
+  ModularDevice::Parameter parameters_[constants::PARAMETER_COUNT_MAX];
+  ModularDevice::Method methods_[constants::METHOD_COUNT_MAX];
+
   Standalone::StandaloneInterface standalone_interface_;
+  Standalone::DisplayLabel display_labels_[constants::DISPLAY_LABEL_COUNT_MAX];
+  Standalone::DisplayVariable display_variables_[constants::DISPLAY_VARIABLE_COUNT_MAX];
+  Standalone::InteractiveVariable interactive_variables_[constants::INTERACTIVE_VARIABLE_COUNT_MAX];
+
   Standalone::InteractiveVariable *channel_int_var_ptr_;
   Standalone::InteractiveVariable *state_int_var_ptr_;
   Standalone::InteractiveVariable *c_int_var_ptr_;
@@ -64,6 +74,9 @@ private:
   Standalone::InteractiveVariable *spike_dur_int_var_ptr_;
   Standalone::InteractiveVariable *hold_duty_int_var_ptr_;
   Standalone::InteractiveVariable *hold_dur_int_var_ptr_;
+
+  uint32_t states_array_[constants::STATE_COUNT];
+
 };
 
 extern Controller controller;
